24e1a3cfa8ca9881b25cdeb2a8304b3ded40a39f
anasssanba1 <sanbaanass73@gmail.com>
2023-06-13 14:04:44 +0200

    first commit

--- app/components/Post.tsx
+++ app/components/Post.tsx
@@ -72,7 +72,6 @@ function Post({ title, imageUrl, id }: { title: any; imageUrl: any; id: any }) {
 					width={"350"}
 					height={"500"}
 					quality={100}
-					unoptimized
 				/>
 			</Link>
 		</div>
